c11d7890eb09c17c21a96cf5d6a0d48302fb48f2
Aabid92 <shaikhaabid608@gmail.com>
2023-03-11 14:18:26 +0530

    Facebook-SQL-Questions

---
+++ facebook-SQL-questions.sql
@@ -0,0 +1,12 @@
+-- Assume you are given the tables below about Facebook pages
+-- and page likes. Write a query to return the page IDs 
+-- of all the Facebook pages that don't have any likes. 
+-- The output should be in ascending order.
+
+--Solutions---
+SELECT P.page_id FROM pages as P
+LEFT JOIN 
+page_likes AS pl 
+ON P.page_id = pl.page_id
+WHERE liked_date is NULL
+ORDER BY page_id ASC;
